2da31f827f17d509a5f55c7506ab9720c32a9c76
sudibianto <sudibianto@gmail.com>
2018-07-25 19:45:51 +0700

    update

---
+++ eds-binaries/dbserver/mariadb1038x86x180724190007/eds-app-settings.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * EasyPHP Devserver: a complete development environment
+ * @author   Laurent Abbal <laurent@abbal.com>
+ * @link     http://www.easyphp.org
+ */
+ 
+$app_settings = array();
+$app_settings = array(
+	'app_type'				=>	"dbserver",
+	'app_type_name'			=>	"database server",
+	'app_name'				=>	"MariaDB",
+	'app_version'			=>	"10.3.8 x86",
+	'app_version_nb'		=>	"10.3.8",	
+	'app_mode'				=>	"x86 (32-bit)",
+	'app_architecture'		=>	"x86",
+	'app_website'			=>	"mariadb.org",
+	'app_website_url'		=>	"https://mariadb.org/",
+	'app_icon'				=>	"eds_icon_mysql.png"
+);
+?>
